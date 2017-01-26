3bf0a414420b75fffe478b2f0d1d9c7407609ecb
Jesse-Opitz <jesse.opitz1@marist.edu>
2017-01-26 18:03:06 -0500

    createList complete writeTokens reads chars and ints differently
    Creates list of every character in the code in createList(fileName).
    writeTokens(fileList) identifies and creates tokens for characters and
    integers.

---
+++ getTokens.py
@@ -0,0 +1,51 @@
+import re
+
+def main():
+    #file = input('Which file would you like to compile?');
+    file = 'codeHere.txt'
+    brokenText = createList(file)    
+    writeTokens(brokenText)
+    
+
+def createList(fileName):
+    inFile = open(fileName, 'rU')
+    chars = []
+    for line in inFile:
+        for c in line:
+            chars.append(c)
+    return chars
+
+# '/n' is new line
+# '/t' is tab
+def writeTokens(fileList):
+    resultFile = open('tokens.txt', 'w')
+    tokens = []
+    lineNum = 1
+    #print(fileList[0:])
+    for c in fileList:
+        #print(lineNum, c)
+        lineNum = lineNum + 1
+        if re.match(r'[0-9]', c, 0):
+            num = token('integer', lineNum)
+            print('int', c)
+            tokens.append(num)
+        elif re.match(r'[a-zA-Z]', c, 0):
+            alpha = token('alphabetic', lineNum)
+            print('alpha', c)
+            tokens.append(alpha)
+    #for a in range(0,len(tokens),1):
+    #    print(tokens[0].kind)
+    
+        
+        
+    #print(tokens)
+    #resultFile.write(tokens)
+    resultFile.close()
+
+class token:
+    def __init__(self, kind, lineNum):
+        self.kind = kind
+        self.lineNum = lineNum
+        #self.position = position
+
+main()
