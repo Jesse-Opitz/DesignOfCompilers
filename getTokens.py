f59233cfd7867ee01cfaefd5517c0769086c8d9a
Jesse-Opitz <jesse.opitz1@marist.edu>
2017-01-30 10:01:19 -0500

    Altered tokens to try and make it read each line

--- getTokens.py
+++ getTokens.py
@@ -3,50 +3,49 @@ import re
 def main():
     #file = input('Which file would you like to compile?');
     file = 'codeHere.txt'
-    brokenText = createList(file)    
-    writeTokens(brokenText)
-    
-
-def createList(fileName):
-    inFile = open(fileName, 'rU')
-    chars = []
+    inFile = open(file, 'rU')
+    tokens = []
+    lineNum = 0
     for line in inFile:
-        for c in line:
-            chars.append(c)
-    return chars
+        #print(line)
+        tokens.append(writeTokens(line, lineNum))
+        
+
+    resultFile = open('tokens.txt', 'w')
+    for a in range(0,len(tokens),1):
+        print(tokens[a].kind + ' ' + str(tokens[a].character) + ' ' + str(tokens[a].lineNum))
+        
+    #resultFile.write(tokens[a].kind + ' ' + tokens[a].character + ' ' + str(tokens[a].lineNum) + '\n')
+    resultFile.close()
+
+#def createList(fileName):
+#    inFile = open(fileName, 'rU')
+#    chars = []
+#    for line in inFile:
+#        for c in line:
+#            chars.append(c)
+#    return chars
 
 # '/n' is new line
 # '/t' is tab
-def writeTokens(fileList):
-    resultFile = open('tokens.txt', 'w')
-    tokens = []
-    lineNum = 1
-    #print(fileList[0:])
-    for c in fileList:
-
-        # Increments line number everytime a \n or new line character is entered
-        if c is "\n":
-            lineNum = lineNum + 1
-        
-        if re.match(r'[0-9]', c, 0):
-            num = token('integer', lineNum)
-            print('int', c, lineNum)
-            tokens.append(num)
-        elif re.match(r'[a-zA-Z]', c, 0):
-            alpha = token('alphabetic', lineNum)
-            print('alpha', c, lineNum)
-            tokens.append(alpha)
-    #for a in range(0,len(tokens),1):
-    #    print(tokens[0].kind)
-    
-        
+def writeTokens(line, lineNum):
+    if re.match(r'[0-9]', line, 0):
+        num = token(1, 'integer', lineNum)
+        #print('int', c, lineNum)
+        return num
+    elif re.match(r'[a-zA-Z]', line, 0):
+        alpha = token(2, 'alphabetical', lineNum)
+        #print('alpha', c, lineNum)
+        #tokens.append(alpha)
+        return alpha
         
     #print(tokens)
     #resultFile.write(tokens)
-    resultFile.close()
+    
 
 class token:
-    def __init__(self, kind, lineNum):
+    def __init__(self, character, kind, lineNum):
+        self.character = character
         self.kind = kind
         self.lineNum = lineNum
         #self.position = position
