b55cf0ac7d92946d9955ba74f1525f480f71f0b1
Jesse-Opitz <jesse.opitz1@marist.edu>
2017-02-06 12:12:06 -0500

    Seperates each peice of grammar into a list

--- lexer.py
+++ lexer.py
@@ -5,31 +5,45 @@ def main():
     #file = input('Which file would you like to compile?');
     file = 'codeHere.txt'
     brokenText = createList(file)
-    print(str(brokenText))
+    #print(str(brokenText))
     writeTokens(brokenText)
     
 # Creates a list of every character in a file
 def createList(file):
-    #inFile = open(fileName, 'rU')
-    chars = [re.findall(r'[a-z][a-z]+|[a-z]|[0-9]|[=][=]|[(){}=]|[!][=]|["][a-z]+["]|[\n]|[\t]|[+]|[$]',line) 
-            for line in open(file)]
+    inFile = open(file, 'r')
+    #chars = [re.findall(r'[a-z][a-z]+|[a-z]|[0-9]|[=][=]|[(){}=]|[!][=]|["][a-z]+["]|[\n]|[\t]|[+]|[$]',line) 
+    #        for line in open(file)]
+    pattern = re.compile(r'[a-z][a-z]+|[a-z]|[0-9]|[=][=]|[(){}=]|[!][=]|["][a-z]+["]|[\n]|[\t]|[+]|[$]')
+    chars = []
+    for line in inFile:
+        chars.append(pattern.findall(line))
+        
     return chars
+
+
 
 # '/n' is new line
 # '/t' is tab
 # Goes through a list for specific grammer
+# Line numbers start at 0 because we are programmers
 def writeTokens(charList):
     resultFile = open('tokens.txt', 'w')
     tokens = []
     lineNum = 1
-    tempChar = []
     seperators = [' ', '\n', '\t', '=', '==']
     specialStatements = ['if', 'while', 'print', 'int', 'string', 'boolean', 'false', 'true']
-    
+    line = 0
+    word = 0
+    while line < len(charList)-1:
+        print(charList[line])
+        line = line + 1
+        #while word < len(charList[0:]) - 1 :
+            #word = word + 1
+            #print(charList[line][word])
 
-    for a in range(0,len(tokens),1):
-        print(tokens[a].kind + ' ' + tokens[a].character + ' ' + str(tokens[a].lineNum) + '')
-        resultFile.write(tokens[a].kind + ' ' + tokens[a].character + ' ' + str(tokens[a].lineNum) + '\n')
+    #for a in range(0,len(tokens),1):
+        #print(tokens[a].kind + ' ' + tokens[a].character + ' ' + str(tokens[a].lineNum) + '')
+        #resultFile.write(tokens[a].kind + ' ' + tokens[a].character + ' ' + str(tokens[a].lineNum) + '\n')
     
     resultFile.close()
 
