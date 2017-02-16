63ac03392c4d65b82673124bfd17cd3fecf60f3b
Jesse-Opitz <jesse.opitz1@marist.edu>
2017-02-16 10:39:24 -0500

    Fixed all issues in lexer and added an errors.txt file that takes in any errors

--- project/lexer.py
+++ project/lexer.py
@@ -13,14 +13,15 @@ def main():
         #print(str(brokenText))
         writeTokens(wordList, errors)
     #print('Errors: ' + str(errors))
+    errorFile = open('errors.txt', 'w')
     while e < len(errors):
         print(errors[e])
+        errorFile.write(str(errors[e]) + '\n')
         e = e + 1
+    errorFile.close()
 
     input('Press enter to end')
 
-
-    
 # Creates a list of every character in a file
 def createList(file):
     inFile = open(file, 'r')
@@ -81,7 +82,7 @@ def writeTokens(wordList, errors):
     line = 0
     word = 0
     while line < len(wordList):
-        print(line, wordList[line])
+        #print(line, wordList[line])
         #print(wordList[line], line)
         #print(len(wordList[line]))
         beforeQuote = True
@@ -93,64 +94,71 @@ def writeTokens(wordList, errors):
                     newTok = token('keyword', uncheckedWord, line)
                     tokens.append(newTok)
                 else:
-                    print('Error: unexpected syntax "', uncheckedWord, '" on line', line)
-                    errorLine = 'Error: Unexpected syntax ' + str(uncheckedWord) + ' at line' + str(lineNum)
+                    #print('Error: unexpected syntax "', uncheckedWord, '" on line', line)
+                    errorLine = 'Error: Unexpected syntax ' + str(uncheckedWord) + ' at line ' + str(line)
                     errors.append(errorLine)
             elif re.match(charPattern, uncheckedWord, 0)  and quoteCount % 2 == 0:
-                newTok = token('char', uncheckedWord, line)
+                newTok = token('char', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(numPattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('digit', uncheckedWord, line)
+                newTok = token('digit', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(comparePattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('compare', uncheckedWord, line)
+                newTok = token('compare', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(assignPattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('assign', uncheckedWord, line)
+                newTok = token('assign', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(bracketPattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('bracket', uncheckedWord, line)
+                newTok = token('bracket', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(parenPattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('paren', uncheckedWord, line)
+                newTok = token('paren', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(operatorPattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('operator', uncheckedWord, line)
+                newTok = token('operator', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(quotePattern, uncheckedWord, 0):
                 quoteCount = quoteCount + 1
-                newTok = token('quote', uncheckedWord, line)
+                newTok = token('quote', uncheckedWord, line + 1)
                 tokens.append(newTok)
             elif re.match(eopPattern, uncheckedWord, 0) and quoteCount % 2 == 0:
-                newTok = token('endProgram', uncheckedWord, line)
+                newTok = token('endProgram', uncheckedWord, (line + 1))
                 tokens.append(newTok)
-            elif re.match(nonTokenBlankSpacePattern, uncheckedWord, 0):
+            elif re.match(nonTokenBlankSpacePattern, uncheckedWord, 0) and quoteCount % 2 == 0:
                 continue
             elif quoteCount % 2 == 1:
                 #print(uncheckedWord)
-                for c in uncheckedWord:
-                    print('first c is ' + c)
-                    if c is '"':
-                        beforeQuote = False
-                        print('qc is ' + c)
-                    elif c is not '"':
-                        print('c is ' + c)
-                        if re.match(r'[a-z]', c, 0):
-                            #print('Here1 ' + str(line))
-                            newTok = token('char', c, line)
-                            #print('Here2 ' + str(line))
-                            tokens.append(newTok)
-                        elif re.match(r'[ ]', c, 0):
-                            print('space')
-                            newTok = token('char', '\s', line)
-                            tokens.append(newTok)
-                        else:
-                            errorLine = 'Error: Unexpected syntax ' + str(uncheckedWord) + ' at line ' + str(lineNum)
-                            errors.append(errorLine)
-                    print(errorLine)
+                #print('first c is ' + c)
+                #print(uncheckedWord)
+                if uncheckedWord is '"':
+                    beforeQuote = False
+                    #print('qc is ' + c)
+                elif uncheckedWord is not '"':
+                    #print('c is ' + c)
+                    if re.match(r'[a-z]+', uncheckedWord, 0):
+                        #print('Here1 ' + str(line))     # This line shows correct line num
+                        newTok = token('char', uncheckedWord, str(line + 1)) # This line shows as 1
+                        #print('Here2 ' + str(line))    # This line shows correct line num
+                        tokens.append(newTok)
+                    elif re.match(r'[ ]', uncheckedWord, 0):
+                        #print('space')
+                        newTok = token('char', '\s', line + 1)
+                        tokens.append(newTok)
+                    elif re.match(r'[\n]', uncheckedWord, 0):
+                        errorLine = 'Error: Unexpected \\n in a string, at line ' + str(line + 1)
+                        errors.append(errorLine)
+                    elif re.match(r'[\t]', uncheckedWord, 0):
+                        errorLine = 'Error: Unexpected tab in a string, at line ' + str(line + 1)
+                        errors.append(errorLine)
+                    else:
+                        #print('Else Here' + str(line))
+                        errorLine = 'Error: Unexpected syntax in a string, ' + str(uncheckedWord) + ' at line ' + str(line + 1)
+                        errors.append(errorLine)
+                        #print(errorLine)
             else:
                 if quoteCount % 2 == 0:
-                    print('Error: unexpected syntax', uncheckedWord, 'on line ', line)
+                    print('Error: unexpected syntax', uncheckedWord, 'on line ', str(line + 1))
         line = line + 1
             
         
